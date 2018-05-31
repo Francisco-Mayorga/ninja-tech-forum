cfdf9d5fb25ff4f740b885408bda3acd652dd8da
Francisco Mayorga <fffmayorga@gmail.com>
2018-05-31 15:02:30 +0200

    home

---
+++ handlers/home.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from handlers.base import BaseHandler
+from models.topic import Topic
+
+class MainHandler(BaseHandler):
+    def get(self):
+
+        topics = Topic.query(Topic.deleted == False).fetch()
+        context = {"topics": topics}
+        return self.render_template("main.html", params=context)
