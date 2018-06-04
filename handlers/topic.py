0473232f48da9ed6b400fd7f474977ae709e7f98
Francisco Mayorga <fffmayorga@gmail.com>
2018-06-04 20:25:59 +0200

    Prueba de segundo repo

--- handlers/topic.py
+++ handlers/topic.py
@@ -1,16 +1,37 @@
+import uuid
+
 from handlers.base import BaseHandler
-from google.appengine.api import users
+from google.appengine.api import users, memcache
 from models.topic import Topic
 
 class TopicAddHandler(BaseHandler):
     def get(self):
-        return self.render_template("topic_add.html")
+        logged_user = users.get_current_user()
+        if not logged_user:
+            return self.write("Please login before you're allowed to post a topic.")
+
+
+        csrf_token = str(uuid.uuid4())
+        memcache.add(key="csrf_token", value=logged_user.email(), time=600)  # "time" means how many seconds will item be in memcache
+        context = {
+            "csrf_token": csrf_token,
+        }
+
+        return self.render_template("topic_add.html", params=context)
 
     def post(self):
         logged_user = users.get_current_user()
 
         if not logged_user:
             return self.write("Please login before you're allowed to post a topic.")
+
+        csrf_token = self.request.get("csrf-token")
+        mem_token =memcache.get(key=csrf_token)
+
+        if not mem_token or mem_token != logged_user.email():
+            return self.write("You are evil attacker...")
+
+
 
         title_value = self.request.get("title")
         text_value = self.request.get("text")
