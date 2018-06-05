3bbca06dfa9ba9793b92ebdd7d6ae362e4687826
Francisco Mayorga <fffmayorga@gmail.com>
2018-06-05 12:30:02 +0200

    corrigiendo la clave csrf

--- handlers/topic.py
+++ handlers/topic.py
@@ -4,15 +4,16 @@ from handlers.base import BaseHandler
 from google.appengine.api import users, memcache
 from models.topic import Topic
 
+
 class TopicAddHandler(BaseHandler):
     def get(self):
         logged_user = users.get_current_user()
         if not logged_user:
             return self.write("Please login before you're allowed to post a topic.")
 
-
         csrf_token = str(uuid.uuid4())
-        memcache.add(key="csrf_token", value=logged_user.email(), time=600)  # "time" means how many seconds will item be in memcache
+        memcache.add(key=csrf_token, value=logged_user.email(),
+                     time=600)  # "time" means how many seconds will item be in memcache
         context = {
             "csrf_token": csrf_token,
         }
@@ -20,17 +21,16 @@ class TopicAddHandler(BaseHandler):
         return self.render_template("topic_add.html", params=context)
 
     def post(self):
-        csrf_token = self.request.get("csrf-token")
-        mem_token =memcache.get(key=csrf_token)
-
-        if not mem_token or mem_token != logged_user.email():
-            return self.write("You are evil attacker...")
-
-
         logged_user = users.get_current_user()
 
         if not logged_user:
             return self.write("Please login before you're allowed to post a topic.")
+
+        csrf_token = self.request.get("csrf-token")
+        mem_token = memcache.get(key=csrf_token)
+
+        if not mem_token or mem_token != logged_user.email():
+            return self.write("You are evil attacker...")
 
         title_value = self.request.get("title")
         text_value = self.request.get("text")
@@ -50,10 +50,3 @@ class TopicAddHandler(BaseHandler):
         new_topic.put()
 
         return self.redirect_to("topic-details", topic_id=new_topic.key.id())
-
-
-
-
-
-
-
